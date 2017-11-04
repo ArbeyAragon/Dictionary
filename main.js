d4a254a2f75b599f87a42f1d19e7a7bd7ceab8ad
Arbey Aragon <arbey.aragon@gmail.com>
2017-11-04 03:31:42 -0500

    Cambio de titulo

--- main.js
+++ main.js
@@ -86,8 +86,8 @@ function setData(data){
     let datos = ''
     let  len = data.length-1;
     datos = datos + `<button type="button" class="btn btn-secondary" onclick="udoStatus()">Udo</button> 
-        <p><a href="#" id="count_win" class="text-success">0</a></p>
-        <p><a href="#" id="count_dan" class="text-danger">0</a></p>
+        <p><a class="text-success">Win: </a><a href="#" id="count_win" class="text-success">0</a></p>
+        <p><a class="text-danger">Losse: </a><a href="#" id="count_dan" class="text-danger">0</a></p>
     <div class="row">`;
         datos=datos+'<div class="btn-toolbar justify-content-between" role="toolbar" aria-label="Toolbar with button groups">';
         for(var i = 0 ; i <= len ; i++){
